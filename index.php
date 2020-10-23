0bb8690506f19659c836dac82123c278101939d3
mariojbe <>
2020-10-23 13:44:58 -0300

    Esse é meu segundo commit

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Teste</title>
+</head>
 
+<body>
 
+    Este é minha modificação.
+
+</body>
+
+</html>
