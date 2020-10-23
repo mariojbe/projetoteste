24686cfc5cfc16ddcc3302a5a31d4c5a20e0657c
mariojbe <>
2020-10-23 13:46:17 -0300

    Este é meu quarto commit.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Teste</title>
+    <title>Versionamento GIT</title>
 </head>
 
 <body>
